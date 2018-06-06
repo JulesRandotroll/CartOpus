0c6ce02280185f8957e3f04451b665819241b160
JulesRandotroll <jules.gc22120@gmail.com>
2018-06-06 15:32:59 +0200

    Base2.2

--- application/controllers/Visiteur.php
+++ application/controllers/Visiteur.php
@@ -1,18 +1,24 @@
 <?php 
 
-class Visiteur extends CI_Controller {
+class Visiteur extends CI_Controller 
+{
 
     public function __construct()
     {
        parent::__construct();
        $this->load->helper('url');
        $this->load->helper('assets'); // helper 'assets' ajouté a Application
-       //$this->load->library("pagination");
+       $this->load->library("pagination");
        //$this->load->model('ModeleArticle'); // chargement modèle, obligatoire
        //$this->load->model('ModeleUtilisateur');
     } // __construct
 
-
+    public function loadAccueil()
+    {
+        $this->load->view('templates/Entete');
+        $this->load->view('Visiteur/Accueil');
+        $this->load->view('templates/PiedDePage');
+    }
 
 
 }//Fin Visiteur
