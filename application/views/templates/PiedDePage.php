37b58d28f67992c745ac5d77769dc3c709a8867f
JulesRandotroll <jules.gc22120@gmail.com>
2018-06-07 17:47:02 +0200

    img1.0

--- application/views/templates/PiedDePage.php
+++ application/views/templates/PiedDePage.php
@@ -1,3 +1,9 @@
-<p><em>&copy; MJC du Plateau</em></p>
+
+    <div class="row" style="background-color:#B64F53">
+        <section>
+            <div class = "section-inner"> 
+                <div class="col-sm-2" style="padding:20px">
+                    <p><em>&copy; MJC du Plateau</em></p>
+    </div>
 </body>
 </html>
