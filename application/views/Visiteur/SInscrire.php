93f02dd8202ac15eef95476417f0f8c0c61aa885
JulesRandotroll <jules.gc22120@gmail.com>
2018-06-11 18:14:21 +0200

    MortAuxEspacesBlancs

--- application/views/Visiteur/SInscrire.php
+++ application/views/Visiteur/SInscrire.php
@@ -1,75 +1,73 @@
 <html>
 <body>
-<div class = "text-center">
-                <BR>
-                <?php 
-                    //echo'<a href="'.site_url('Visiteur/SInscrire').'" class="btn btn-danger" > S\'inscrire</a>   ';
-                    echo'<a href="'.site_url('Visiteur/SeConnecter').'" class="btn btn-danger" > Se connecter</a>';
-                ?>  
+                <div class = "text-center">
+                    <BR>
+                    <?php 
+                        //echo'<a href="'.site_url('Visiteur/SInscrire').'" class="btn btn-danger" > S\'inscrire</a>   ';
+                        echo'<a href="'.site_url('Visiteur/SeConnecter').'" class="btn btn-danger" > Se connecter</a>';
+                    ?>  
                 </div>
             </div>
         </div>
     </section>
 </div>
-<?php
-    echo validation_errors(); // mise en place de la validation
-    echo form_open('visiteur/sInscrire');
-?>
 <div class="row" style="background-color:#15B7D1">
-        <div class="col-sm-3">
-        </div>
-        <div class="col-sm-6" style="padding:20px">
-                <div style="padding:20px">
-                    <div class = "text-center">
-                        <H1 style="color:#FFFFFF">Inscription<H1>
-                        <section >
-                            <div class = "section-inner" style="background-color:#139CBC;padding:20px">
-                                    <?php
-                                        echo('<table class="table" border=0>');
+    <div class="col-sm-3">
+    </div>
+    <div class="col-sm-6" style="padding:20px">
+            <div style="padding:20px">
+                <div class = "text-center">
+                    <H1 style="color:#FFFFFF">Inscription<H1>
+                    <section >
+                        <div class = "section-inner" style="background-color:#139CBC;padding:20px">
+                                <?php
+                                    echo validation_errors(); // mise en place de la validation
+                                    echo form_open('visiteur/sInscrire');
+                                    echo('<table class="table" border=0>');
 
-                                        echo('<tr><td>');
-                                        echo('Nom: ');
-                                        echo('</td><td>');
-                                        echo form_input('nom',$nom,array('required'=>'required','pattern'=>'[a-zA-Z]{1,40}')); 
-                                        echo('</td></tr>');
+                                    echo('<tr><td>');
+                                    echo('Nom: ');
+                                    echo('</td><td>');
+                                    echo form_input('nom',$nom,array('required'=>'required','pattern'=>'[a-zA-Z]{1,40}')); 
+                                    echo('</td></tr>');
 
-                                        echo('<tr><td>');
-                                        echo ('Prénom: ');
-                                        echo('</td><td>');
-                                        echo form_input('prenom',$prenom,array('pattern'=>'[a-zA-Z]{1,20}'));                                 echo('</td></tr>');
+                                    echo('<tr><td>');
+                                    echo ('Prénom: ');
+                                    echo('</td><td>');
+                                    echo form_input('prenom',$prenom,array('pattern'=>'[a-zA-Z]{1,20}'));                                 echo('</td></tr>');
 
-                                        echo('<tr><td>');
-                                        echo ('Email d\'identification: '); // creation d'un label devant la zone de saisie
-                                        echo('</td><td>');
-                                        echo form_input('mail',$mail,array('required'=>'required','pattern'=>'[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([_\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})'));
-                                        echo('</td></tr>');
+                                    echo('<tr><td>');
+                                    echo ('Email d\'identification: '); // creation d'un label devant la zone de saisie
+                                    echo('</td><td>');
+                                    echo form_input('mail',$mail,array('required'=>'required','pattern'=>'[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([_\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})'));
+                                    echo('</td></tr>');
 
-                                        echo('<tr><td>');
-                                        echo ('Téléphone du responsable: '); // creation d'un label devant la zone de saisie
-                                        echo('</td><td>');                                echo form_input('tel',$tel,array('pattern'=>'[0-9]{10}'));
-                                        echo('</td></tr>');
+                                    echo('<tr><td>');
+                                    echo ('Téléphone du responsable: '); // creation d'un label devant la zone de saisie
+                                    echo('</td><td>');                                echo form_input('tel',$tel,array('pattern'=>'[0-9]{10}'));
+                                    echo('</td></tr>');
 
-                                        echo('<tr><td>');
-                                        echo ('Mot de Passe: '); // creation d'un label devant la zone de saisie
-                                        echo('</td><td>');
-                                        echo form_password('mdp','',array('required'=>'required')).$message;
-                                        echo('</td></tr>');
+                                    echo('<tr><td>');
+                                    echo ('Mot de Passe: '); // creation d'un label devant la zone de saisie
+                                    echo('</td><td>');
+                                    echo form_password('mdp','',array('required'=>'required')).$message;
+                                    echo('</td></tr>');
 
-                                        echo('<tr><td>');
-                                        echo ('Confirmation du mot de passe: '); // creation d'un label devant la zone de saisie
-                                        echo('</td><td>');
-                                        echo form_password('confmdp','',array('required'=>'required'));// VERIF si confirme == mdp
-                                        echo('</td></tr>');
+                                    echo('<tr><td>');
+                                    echo ('Confirmation du mot de passe: '); // creation d'un label devant la zone de saisie
+                                    echo('</td><td>');
+                                    echo form_password('confmdp','',array('required'=>'required'));// VERIF si confirme == mdp
+                                    echo('</td></tr>');
 
-                                        echo('<tr><td>');
-                                        echo('</td><td>');
-                                        echo form_submit('valider', 'Valider l\'inscription');
-                                        echo('</td></tr>');
-                                        echo('</table>');
+                                    echo('<tr><td>');
+                                    echo('</td><td>');
+                                    echo form_submit('valider', 'Valider l\'inscription');
+                                    echo('</td></tr>');
+                                    echo('</table>');
 
                                                     //echo form_submit('retour', 'Retour');
-                                        echo form_close();
-                                    ?>
+                                    echo form_close();
+                                ?>
                             </div>
                         <section>
                     </div>
@@ -78,6 +76,3 @@
         </div>
     </div>
 </div>
-
-</body>
-<html>
--- application/views/Visiteur/SeConnecter.php
+++ application/views/Visiteur/SeConnecter.php
@@ -1,6 +1,6 @@
 <html>
 <body>
-<div class = "text-center">
+            <div class = "text-center">
                 <BR>
                 <?php 
                     echo'<a href="'.site_url('Visiteur/SInscrire').'" class="btn btn-danger" > S\'inscrire</a>   ';
@@ -12,42 +12,35 @@
     </section>
 </div>
 <div class="row" style="background-color:#15B7D1"> 
-
     <div class="col-sm-2">
     </div> 
     <div class="col-sm-8">
         <div class = "text-center">
             <H1 align = "center" style="color:#FFFFFF">Connexion</H1><BR>;
             <section >
-                    <div class = "section-inner" style="background-color:#139CBC;padding:20px">
-                        <?php
-                       
-                            echo validation_errors(); // mise en place de la validation
+                <div class = "section-inner" style="background-color:#139CBC;padding:20px">
+                    <?php   
+                        echo validation_errors(); // mise en place de la validation
                                 /* set_value : en cas de non validation les données déjà
                                 saisies sont réinjectées dans le formulaire */
                             
-                            echo form_open('Visiteur/seConnecter');
-                            echo form_label('Mail : ','mail'); // creation d'un label devant la zone de saisie
-                            echo form_input('mail','',array('required'=>'required'));
-                            echo' ';
-                            echo form_label('Mot de passe : ','mdp');
-                            echo form_password('mdp','',array('required'=>'required'));
-                            echo' ';
-                            echo form_submit('submit', 'Se connecter');
-                            echo' ';
-                            echo '<a href="'.site_url('Visiteur/RecupMDP').'">Mot de passe oublié ?</a>';
-                            echo form_close();
-
-                            echo '<a href="'.site_url('Visiteur/sInscrire').'">S\'inscrire ? </a>';
-                        
-                        ?>
-                    </div>
+                        echo form_open('Visiteur/seConnecter');
+                        echo form_label('Mail : ','mail'); // creation d'un label devant la zone de saisie
+                        echo form_input('mail','',array('required'=>'required'));
+                        echo' ';
+                        echo form_label('Mot de passe : ','mdp');
+                        echo form_password('mdp','',array('required'=>'required'));
+                        echo' ';
+                        echo form_submit('submit', 'Se connecter');
+                        echo' ';
+                        echo '<a href="'.site_url('Visiteur/RecupMDP').'">Mot de passe oublié ?</a>';
+                        echo form_close();
+                        echo '<a href="'.site_url('Visiteur/sInscrire').'">S\'inscrire ? </a>';
+                    ?>
+                </div>
             </section>
+            <br>
         </div>
     </div>
-    <div class="col-sm-2">
-    </div> 
 </div>
 
-</body>
-<html>
