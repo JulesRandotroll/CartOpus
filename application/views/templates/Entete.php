5259ffd94ac3161b685cc1a0431f8a53454cfc0e
JulesRandotroll <jules.gc22120@gmail.com>
2018-06-11 16:58:18 +0200

    InsertionsBDD

--- application/views/templates/Entete.php
+++ application/views/templates/Entete.php
@@ -5,20 +5,18 @@
     </head>
     <body>
     <div class="row" style="background-color:#0E7896">
-    <section>
-        <div class = "section-inner"> 
-            <div class="col-sm-2" style="padding:20px">
-                <?php 
-                    echo'<a href="'.site_url('Visiteur/loadAccueil').'">'.img('logoAccueil.png').'</a>';
-                ?>
-            </div>
-            <div class="col-sm-7" style="padding:20px">
-                <div class = "text-center">
+        <section>
+            <div class = "section-inner"> 
+                <div class="col-sm-2" style="padding:20px">
                     <?php 
-                        echo img('Banderole.png');
+                        echo'<a href="'.site_url('Visiteur/loadAccueil').'">'.img('logoAccueil.png').'</a>';
                     ?>
                 </div>
-            </div>
-            <div class="col-sm-3" style="padding:20px">
-
-   
+                <div class="col-sm-7" style="padding:20px">
+                    <div class = "text-center">
+                        <?php 
+                            echo img('Banniere.png');
+                        ?>
+                    </div>
+                </div>
+                <div class="col-sm-3" style="padding:20px">
