655c8426c666ef57092203c7765980321b88270a
JulesRandotroll <jules.gc22120@gmail.com>
2018-06-12 16:05:42 +0200

    ploploplop

--- application/views/Visiteur/Accueil.php
+++ application/views/Visiteur/Accueil.php
@@ -9,31 +9,51 @@
         </div>
     </section>
 </div>
-<div class="row" style="background-color:#15B7D1">
-    <div class="col-sm-1" style="padding:20px">
+
+
+ <div class="row" style="background-color:#15B7D1;padding:20px">
+    
+    <div class="col-sm-1">
+        <?php 
+                echo form_open('Visiteur/GetActionRecherchee'); 
+        ?>
     </div>
-    <div class="col-sm-10" style="padding:20px">
+    <div class="col-sm-3" style="padding:10px">
         <div class = "text-center">
             <section >
-                <div class = "section-inner" style="padding:20px">
-                    <?php echo form_open('Visiteur/GetActionRecherchee'); ?>
+                <div class = "section-inner" style="padding:10px">
+                    <div class="form-group">
                         <?php 
-                             
-                            //echo 'Rechercher : ';
-                            echo 
-                            form_label('Rechercher  ', 'lbl_Recherche');
+                            echo form_label('Rechercher :', 'lbl_Recherche');
+                            echo '  ';
+                            echo form_input('MotCle', '', array('placeholder'=>'Rechercher'));
                             echo ' ';
-                            echo form_input('MotCle', '', array('placeholder'=>'Rechercher')).' ';
+                        ?>
+                    </div>
+                    <div class="form-group">
+                        <?php
+                            echo form_label('Thématique :', 'lbl_Thematique');
                             echo ' ';
-                            echo form_label('Thématique ', 'lbl_Thematique');
                             $option = array(
                                 'Musique'=>array('Musique','Rock','Jazz','Blues'),
                                 'Sport'=>array('Sport','Kayak','Karate')
                             );
-                            echo ' ';
                             echo form_dropdown('Thematique', $option, 'default');
                             echo ' ';
-                            echo form_label('Date ', 'lbl_Date');
+                        ?>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+    <div class="col-sm-3" style="padding:10px">
+        <div class = "text-center">
+            <section>
+                <div class = "section-inner" style="padding:10px">
+                    <div class="form-group">
+                        <?php
+                            echo form_label('Date :', 'lbl_Date');
+                            echo '  ';
                             $jour = array(
                                 1=>1,
                                 2=>2,
@@ -67,6 +87,7 @@
                                 30=>30,
                                 31=>31
                             );
+                            
                             $mois = array(
                                 01=>'Janvier',
                                 02=>'Fevrier',
@@ -81,7 +102,9 @@
                                 11=>'Novembre',
                                 12=>'Decembre',
                             );
+                            
                             $AnneeEnCours = date('Y');
+                            
                             $annee = array(
                                 $AnneeEnCours-5=>$AnneeEnCours-5,
                                 $AnneeEnCours-4=>$AnneeEnCours-4,
@@ -91,125 +114,12 @@
                                 $AnneeEnCours=>$AnneeEnCours,
                                 $AnneeEnCours+1=>$AnneeEnCours+1
                             );
-                            echo ' ';
+
                             echo form_dropdown('Jour', $jour, date('d'));
                             echo form_dropdown('Mois', $mois, date('m'));
                             echo form_dropdown('Annee', $annee, date('Y'));
-
-                            echo ' ';
-                            //echo 'Lieu : ';
-                            echo form_label('Lieu  ', 'lbl_Lieu');      
-                            echo ' ';
-                            echo form_input('Lieu', '', array('placeholder'=>'Rechercher'));                
-                            echo ' ';
-                            echo form_submit('submit','Rechercher');
-                            
                         ?>
-                    <?php echo form_close(); ?>
-                </div>
-            </section>
-        </div>
-    </div>
-</div>
-
-
-
-
-
-
-
-
-
-<!-- <div class="row" style="background-color:#15B7D1">
-    <?php echo form_open('Visiteur/GetActionRecherchee'); ?>
-    <div class="col-sm-5" style="padding:10px">
-        <div class = "text-center">
-            <section >
-                <div class = "section-inner" style="padding:10px">
-                    <?php 
-                        echo 'Rechercher : ';
-                        echo form_input('MotCle', '', array('placeholder'=>'Rechercher'));
-                        echo 'Thématique : ';
-                        $option = array(
-                            'Musique'=>array('Musique','Rock','Jazz','Blues'),
-                            'Sport'=>array('Sport','Kayak','Karate')
-                        );
-                        echo form_dropdown('Thematique', $option, 'default');
-                    ?>
-                </div>
-            </section>
-        </div>
-    </div>
-    <div class="col-sm-3" style="padding:10px">
-        <div class = "text-center">
-            <section>
-                <div class = "section-inner" style="padding:10px">
-                    Date : 
-                    <?php
-                        
-                        $jour = array(
-                            1=>1,
-                            2=>2,
-                            3=>3,
-                            4=>4,
-                            5=>5,
-                            6=>6,
-                            7=>7,
-                            8=>8,
-                            9=>9,
-                            10=>10,
-                            11=>11,
-                            12=>12,
-                            13=>13,
-                            14=>14,
-                            15=>15,
-                            16=>16,
-                            17=>17,
-                            18=>18,
-                            19=>19,
-                            20=>20,
-                            21=>21,
-                            22=>22,
-                            23=>23,
-                            24=>24,
-                            25=>25,
-                            26=>26,
-                            27=>27,
-                            28=>28,
-                            29=>29,
-                            30=>30,
-                            31=>31
-                        );
-                        
-                        $mois = array(
-                            01=>'Janvier',
-                            02=>'Fevrier',
-                            03=>'Mars',
-                            04=>'Avril',
-                            05=>'Mai',
-                            06=>'Juin',
-                            07=>'Juillet',
-                            08=>'Aout',
-                            09=>'Septembre',
-                            10=>'Octobre',
-                            11=>'Novembre',
-                            12=>'Decembre',
-                        );
-                        
-                        $AnneeEnCours = date('Y');
-                        
-                        $annee = array($AnneeEnCours-5=>$AnneeEnCours-5,
-                        $AnneeEnCours-4=>$AnneeEnCours-4,
-                        $AnneeEnCours-3=>$AnneeEnCours-3,
-                        $AnneeEnCours-2=>$AnneeEnCours-2,
-                        $AnneeEnCours-1=>$AnneeEnCours-1,
-                        $AnneeEnCours=>$AnneeEnCours,
-                        $AnneeEnCours+1=>$AnneeEnCours+1);
-
-                        echo form_dropdown('Jour', $jour, date('d'));
-                        echo form_dropdown('Mois', $mois, date('m'));
-                        echo form_dropdown('Annee', $annee, date('Y'));
-                    ?>
+                    </div>
                 </div>
             </section>
         </div>
@@ -218,23 +128,38 @@
         <div>
             <section>
                 <div class = "section-inner" style="padding:10px">
-                    <table>
-                        <?php
-                            echo '<tr><td>'; 
-                            echo 'Lieu : ';
-                            echo'</td><td>';
-                            echo form_input('Lieu', '', array('placeholder'=>'Rechercher'));
-                            echo'</td><td>';
-                            echo form_submit('submit','Rechercher');
-                            echo form_close();
-                            echo'</td></tr>'
-                        ?>
-                    </table>
+                    <div class="form-group">
+                       
+                            <?php
+                                
+                                echo form_label('Lieu :', 'lbl_Lieu');
+                                echo '  '; 
+                                
+                                echo form_input('Lieu', '', array('placeholder'=>'Rechercher'));
+                                echo ' ';
+                            ?>
+                       
+                    </div>
                 </div>
             </section>
         </div>
     </div>
-</div> -->
+    <div class="col-sm-1">
+        <div class = "text-center">
+            <section >
+                <div class = "section-inner" style="padding:20px">
+                    <BR>
+                    <div class="form-group">
+                        <?php 
+                            echo form_submit('submit','Rechercher',array('class'=>'btn-danger btn-lg'));
+                            echo form_close();
+                        ?>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+</div>
 
 <div class="row" style="background-color:#15B7D1">
     <div class="col-sm-1" style="padding:20px">
