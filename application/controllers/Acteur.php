891ca42f867fce52f9ac1f96c68679955a22796d
DeborahPlop <deborahgelinhotmail.fr>
2018-06-25 12:07:12 +0200

    date

--- application/controllers/Acteur.php
+++ application/controllers/Acteur.php
@@ -385,5 +385,14 @@ class Acteur extends CI_Controller
                
     }
     
+    public function ModifierAction()
+    {
+        $DonnéesTitre = array('TitreDeLaPage'=>'Modification Action');
+        
+        $this->load->view('templates/Entete',$DonnéesTitre);
+        $this->load->view('Acteur/ModifierAction');
+        $this->load->view('templates/PiedDePage');
+
+    }
 }
 ?>
