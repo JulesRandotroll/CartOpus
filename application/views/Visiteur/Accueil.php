980442867f5b9eb4539bf681471c7081832eebb9
JulesRandotroll <jules.gc22120@gmail.com>
2019-01-07 17:35:44 +0100

    MAJ + Affichage Accueil plus modif du super Admin

--- application/views/Visiteur/Accueil.php
+++ application/views/Visiteur/Accueil.php
@@ -91,66 +91,67 @@
                         <?php
                             echo form_label('Date :', 'lbl_Date');
                             echo '  ';
-                            // $jour = array(
-                                //     1=>1,
-                                //     2=>2,
-                                //     3=>3,
-                                //     4=>4,
-                                //     5=>5,
-                                //     6=>6,
-                                //     7=>7,
-                                //     8=>8,
-                                //     9=>9,
-                                //     10=>10,
-                                //     11=>11,
-                                //     12=>12,
-                                //     13=>13,
-                                //     14=>14,
-                                //     15=>15,
-                                //     16=>16,
-                                //     17=>17,
-                                //     18=>18,
-                                //     19=>19,
-                                //     20=>20,
-                                //     21=>21,
-                                //     22=>22,
-                                //     23=>23,
-                                //     24=>24,
-                                //     25=>25,
-                                //     26=>26,
-                                //     27=>27,
-                                //     28=>28,
-                                //     29=>29,
-                                //     30=>30,
-                                //     31=>31
-                            // );
+                            $jour = array(
+                                1=>1,
+                                2=>2,
+                                3=>3,
+                                4=>4,
+                                5=>5,
+                                6=>6,
+                                7=>7,
+                                8=>8,
+                                9=>9,
+                                10=>10,
+                                11=>11,
+                                12=>12,
+                                13=>13,
+                                14=>14,
+                                15=>15,
+                                16=>16,
+                                17=>17,
+                                18=>18,
+                                19=>19,
+                                20=>20,
+                                21=>21,
+                                22=>22,
+                                23=>23,
+                                24=>24,
+                                25=>25,
+                                26=>26,
+                                27=>27,
+                                28=>28,
+                                29=>29,
+                                30=>30,
+                                31=>31
+                            );
+                            
+                            $mois = array(
+                                01=>'Janvier',
+                                02=>'Fevrier',
+                                03=>'Mars',
+                                04=>'Avril',
+                                05=>'Mai',
+                                06=>'Juin',
+                                07=>'Juillet',
+                                13=>'Aout',
+                                09=>'Septembre',
+                                10=>'Octobre',
+                                11=>'Novembre',
+                                12=>'Decembre',
+                            );
+                            
+                            $AnneeEnCours = date('Y');
+                            
+                            $annee = array(
+                                $AnneeEnCours-5=>$AnneeEnCours-5,
+                                $AnneeEnCours-4=>$AnneeEnCours-4,
+                                $AnneeEnCours-3=>$AnneeEnCours-3,
+                                $AnneeEnCours-2=>$AnneeEnCours-2,
+                                $AnneeEnCours-1=>$AnneeEnCours-1,
+                                $AnneeEnCours=>$AnneeEnCours,
+                                $AnneeEnCours+1=>$AnneeEnCours+1
+                            );
 
-                            // $mois = array(
-                                //     '01'=>'Janvier',
-                                //     '02'=>'Fevrier',
-                                //     '03'=>'Mars',
-                                //     '04'=>'Avril',
-                                //     '05'=>'Mai',
-                                //     '06'=>'Juin',
-                                //     '07'=>'Juillet',
-                                //     '08'=>'Août',
-                                //     '09'=>'Septembre',
-                                //     '10'=>'Octobre',
-                                //     '11'=>'Novembre',
-                                //     '12'=>'Decembre',
-                            // );
-
-                            // $AnneeEnCours = date('Y');
-
-                            // $annee = array(
-                                //     $AnneeEnCours-5=>$AnneeEnCours-5,
-                                //     $AnneeEnCours-4=>$AnneeEnCours-4,
-                                //     $AnneeEnCours-3=>$AnneeEnCours-3,
-                                //     $AnneeEnCours-2=>$AnneeEnCours-2,
-                                //     $AnneeEnCours-1=>$AnneeEnCours-1,
-                                //     $AnneeEnCours=>$AnneeEnCours,
-                                //     $AnneeEnCours+1=>$AnneeEnCours+1
-                            // );
                             $ToDay = date('d/m/Y');
                             echo '<input class="form-control" name="DateDebut" id="date" type="date" value="'.$ToDay.'" required>';
                             // echo form_dropdown('Jour', $jour, date('d'),array('class'=>'form-control'));
